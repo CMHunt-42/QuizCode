847c4745f61dc78a1a3922b8fe01bf2b71575f21
CMHunt-42 <ixm20@txstate.edu>
2025-09-13 16:37:43 -0500

    Added method prototypes and attributes to questions.h

--- questions.h
+++ questions.h
@@ -1,7 +1,7 @@
 /* *************************************************
 *  Name: Caleb Hunt
 *  Program: Quiz Code
-*  Purpose: Defines Questions class and prototypes
+*  Purpose: Defines Question class and prototypes
 *  of methods. Questions class has string attribute
 *  of the question and of the  answer
 ************************************************* */
@@ -9,12 +9,48 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
+#define DEFAULT_STRING ""
+
+#include <string>
+
 class Question {
 public:
     /**********************
     Constructors/Destructor
     ***********************/
     Question();
+	Question(std::string, std::string);
+	
+	/**********************
+    Getters/Accessors
+    ***********************/
+
+    std::string getQuestion();
+    std::string getAnswer();
+	
+	/**********************
+    Setters/Mutators
+    ***********************/
+    void setQuestion(std::string);
+	void setAnswer(std::string);
+
+    /**********************
+    Printing Methods
+    ***********************/
+
+private:
+
+    /**********************
+    Methods
+    ***********************/
+
+
+    /**********************
+    Attributes
+    ***********************/
+    std::string question;
+	std::string answer;
+
 };
 
 
